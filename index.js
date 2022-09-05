1d931212422123418048ea2e882a9e0e761b88e3
George Fernandez <fernandezg704@gmail.com>
2022-09-05 14:07:27 -0400

    commit tree 8c3f9949c0c03f4485eaf7b45bbe3a6acb5aef60

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'George',
+    streetAdress: '849 Flushing'
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj,
+        [key]: value
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    let newEmployee = {...obj};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
